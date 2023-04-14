f296de1ce6d54e59aa7fb34a98bd7a37881f6ec7
Revendra <t.revendra@gmail.com>
2023-04-13 20:08:13 -0400

    Updated Python Foundations

--- Python/Foundations of Python/07 Modules-Packages/demo_package/demo_module_two_add_sub.py
+++ Python/Foundations of Python/07 Modules-Packages/demo_package/demo_module_two_add_sub.py
@@ -1,11 +1,17 @@
 # Function to add two numbers
 
 def demo_add_two(a,b):
-    '''Function to add two numbers'''
+    '''Function to add two numbers
+      call: demo_add_two(4,3)
+      output: 4+3 = 7
+    '''
     return a+b
 
 # Function to subtract two numbers
 
 def demo_sub_two(a,b):
-    '''Function to subtract two numbers'''
+    '''Function to subtract two numbers
+       call: demo_sub_two(4,3)
+       output: 4-3 = 1
+    '''
     return a-b
--- Python/Foundations of Python/07 Modules-Packages/demo_package/sub_demo_package/sub_demo_three_add_sub.py
+++ Python/Foundations of Python/07 Modules-Packages/demo_package/sub_demo_package/sub_demo_three_add_sub.py
@@ -1,11 +1,18 @@
 # Functions to add/subtract two/two numbers
 
 def demo_add_three(a,b,c):
-    '''Function to add three numbers'''
+    '''Function to add three numbers
+       call function as demo_add_three(1,2,3)
+       demo_add_three returns output as 1+2+3 = 6
+    '''
     return a+b+c
 
 # Function to subtract three numbers
 
 def demo_sub_three(a,b,c):
-    '''Function to subtract three numbers'''
+    '''Function to subtract three numbers
+       call function as demo_sub_three(1,2,3)
+       demo_sub_three returns output as 1-2-3 = -4 
+    '''
+    
     return a-b-c
